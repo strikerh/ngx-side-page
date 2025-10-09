9f9565b589adafb35dd72fcdf772b169c742f139
Hazem Mohamed Nabil <striker.h@gmail.com>
2025-10-09 14:33:02 +0300

    Configure Karma for headless CI and stabilize unit tests

--- projects/ngx-side-page/src/lib/side-page.component.spec.ts
+++ projects/ngx-side-page/src/lib/side-page.component.spec.ts
@@ -1,23 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {BehaviorSubject} from 'rxjs';
 
-import { SidePageComponent } from './side-page.component';
+import {SidePageComponent} from './side-page.component';
+import {SidePageInfo, SidePageService} from './side-page.service';
 
 describe('SidePageComponent', () => {
-  let component: SidePageComponent;
-  let fixture: ComponentFixture<SidePageComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SidePageComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(SidePageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
   it('should create', () => {
+    const pages$ = new BehaviorSubject<SidePageInfo[]>([]);
+    const serviceMock = {
+      initiated: false,
+      getSidePage: () => pages$.asObservable(),
+      closeLastSidePage: jasmine.createSpy('closeLastSidePage'),
+    } as Pick<SidePageService, 'getSidePage' | 'closeLastSidePage'> & {initiated: boolean};
+
+    const component = new SidePageComponent(serviceMock as SidePageService);
+
     expect(component).toBeTruthy();
   });
 });
--- projects/ngx-side-page/src/lib/side-page.service.spec.ts
+++ projects/ngx-side-page/src/lib/side-page.service.spec.ts
@@ -1,16 +1,89 @@
-import { TestBed } from '@angular/core/testing';
+import {ApplicationRef} from '@angular/core';
+import {fakeAsync, tick} from '@angular/core/testing';
+import {BehaviorSubject} from 'rxjs';
 
-import { SidePageService } from './side-page.service';
+import {SidePageService} from './side-page.service';
+
+class MockApplicationRef implements Partial<ApplicationRef> {
+  injector = {} as any;
+  viewCount = 0;
+  componentTypes: any[] = [];
+  components: any[] = [];
+  isStable = new BehaviorSubject(true).asObservable();
+
+  attachView(): void {}
+
+  detachView(): void {}
+
+  tick(): void {}
+
+  whenStable(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  destroy(): void {}
+
+  get destroyed(): boolean {
+    return false;
+  }
+}
 
 describe('SidePageService', () => {
   let service: SidePageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SidePageService);
+    service = new SidePageService(new MockApplicationRef() as unknown as ApplicationRef, undefined as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('closes a non-topmost side page without affecting newer pages', fakeAsync(() => {
+    service['initiated'] = true;
+
+    const emissions: string[][] = [];
+    const subscription = service.getSidePage().subscribe((pages) => {
+      emissions.push(pages.map((page) => page.key));
+    });
+
+    const firstRef = service.openSidePage('first', {} as any);
+    service.openSidePage('second', {} as any);
+
+    expect(service.sidePages.map((page) => page.key)).toEqual(['first', 'second']);
+
+    let closedValue: any = undefined;
+    firstRef.afterClosed().subscribe((value) => (closedValue = value));
+
+    service.closeSidePage('first', 'background close');
+
+    expect(service.sidePages.map((page) => page.key)).toEqual(['second']);
+    expect(emissions[emissions.length - 1]).toEqual(['second']);
+
+    tick(300);
+
+    expect(closedValue).toBe('background close');
+
+    subscription.unsubscribe();
+  }));
+
+  it('ignores requests to close unknown side pages', fakeAsync(() => {
+    service['initiated'] = true;
+
+    const emissions: string[][] = [];
+    const subscription = service.getSidePage().subscribe((pages) => {
+      emissions.push(pages.map((page) => page.key));
+    });
+
+    service.openSidePage('first', {} as any);
+
+    service.closeSidePage('missing', 'value');
+
+    expect(service.sidePages.map((page) => page.key)).toEqual(['first']);
+    expect(emissions[emissions.length - 1]).toEqual(['first']);
+
+    tick(300);
+
+    subscription.unsubscribe();
+  }));
 });
